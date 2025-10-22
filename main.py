8685deb232dc204721affd8a153f13b5d0f226a3
Afostyrio <gonzalez.joshua@hotmail.com>
2025-10-22 10:09:39 -0600

    Comandos funcionales: individual, team
    Nuevos comandos para medallistas individuales y por equipos

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pptx.util import Inches
 import os
 import re
 import pandas as pd
+import numpy
 
 # Cleanup function DELETE LATER
 if os.path.isdir("outputs/Presentación.pptx"):
@@ -14,7 +15,9 @@ with open("config", 'r') as instruction_file:
 	instructions = instruction_file.readlines()
 n_instructions = len(instructions)
 
-medallistas_individual = pd.read_csv("inputs/csv/Medallistas Individual.csv", header=1)
+medallistas_individual = pd.read_csv("inputs/csv/Medallistas Individual.csv", header=0)
+medallistas_equipos = pd.read_csv("inputs/csv/Medallistas Equipos.csv", header=0)
+N_PRESIDIUM = 7
 
 # location_match = re.compile("location: (.*)")
 # location = location_match.match(instructions[0]).group(1)
@@ -86,6 +89,80 @@ def add_moment_slide(i: int):
 	if name: slide.placeholders[0].text = name
 	# if image: slide.placeholders[1].insert_picture(image)
 
+def add_individual_medals(i: int):
+	i += 1
+	if i >= n_instructions: return None
+
+	# Argument seeker
+	while instructions[i][0] == "+":
+		level_match = re.compile("\+ level: (.*)")
+		level_get = level_match.match(instructions[i])
+		if level_get: level = level_get.group(1)
+
+		medal_match = re.compile("\+ medal: (.*)")
+		medal_get = medal_match.match(instructions[i])
+		if medal_get: medal = medal_get.group(1)
+
+		i+=1
+		if i >= n_instructions: break
+	
+	if not medal or not level: return None
+
+	section_slide = prs.slides.add_slide(prs.slide_layouts[2])
+	section_slide.placeholders[0].text = f"Medallas de {medal}\nNivel {level}"
+
+	medallistas = medallistas_individual[(medallistas_individual["Medalla"] == medal) & (medallistas_individual["Nivel"] == level)].values
+	blocks = [medallistas[i:i+N_PRESIDIUM] for i in range(0,len(medallistas), N_PRESIDIUM)]
+	for block in blocks:
+		for j in range(len(block)):
+			medal_slide = prs.slides.add_slide(prs.slide_layouts[8])
+			for k in range(len(block)):
+				medal_slide.placeholders[0].text = f"Medallas de {medal}\nNivel {level}".upper()
+				tf = medal_slide.placeholders[2].text_frame
+				p = tf.add_paragraph()
+				p.text = f"{block[k,3].upper()} ({block[k,0].upper()})"
+				if j==k:
+					p.font.bold = True
+
+
+def add_team_medals(i: int):
+	i += 1
+	if i >= n_instructions: return None
+
+	# Argument seeker
+	while instructions[i][0] == "+":
+		level_match = re.compile("\+ level: (.*)")
+		level_get = level_match.match(instructions[i])
+		if level_get: level = level_get.group(1)
+
+		i+=1
+		if i >= n_instructions: break
+	
+	if not level: return None
+	
+	section_slide = prs.slides.add_slide(prs.slide_layouts[2])
+	section_slide.placeholders[0].text = f"Medallas por equipos\nNivel {level}"
+
+	medallistas = medallistas_equipos[medallistas_equipos["Nivel"]==level]
+	medallistas_bronce = medallistas_equipos[(medallistas_equipos["Nivel"]==level) & (medallistas_equipos["Medalla"] == "Bronce")].values
+	for team in medallistas_bronce:
+		slide = prs.slides.add_slide(prs.slide_layouts[8])
+		slide.placeholders[0].text = "TERCER LUGAR"
+		slide.placeholders[2].text = team[0]
+
+	medallistas_plata = medallistas_equipos[(medallistas_equipos["Nivel"]==level) & (medallistas_equipos["Medalla"] == "Plata")].values
+	for team in medallistas_plata:
+		slide = prs.slides.add_slide(prs.slide_layouts[8])
+		slide.placeholders[0].text = "SEGUNDO LUGAR"
+		slide.placeholders[2].text = team[0]
+
+	medallistas_oro = medallistas_equipos[(medallistas_equipos["Nivel"]==level) & (medallistas_equipos["Medalla"] == "Oro")].values
+	for team in medallistas_oro:
+		slide = prs.slides.add_slide(prs.slide_layouts[8])
+		slide.placeholders[0].text = "PRIMER LUGAR"
+		slide.placeholders[2].text = team[0]
+		
+
 # Main DSL parsing loop
 for i in range(n_instructions):
 	if instructions[i][0] == "#": continue # Ignore comments
@@ -95,6 +172,11 @@ for i in range(n_instructions):
 		add_person_slide(i)
 	if re.match("moment", instructions[i]):
 		add_moment_slide(i)
+	if re.match("individual", instructions[i]):
+		add_individual_medals(i)
+	if re.match("team", instructions[i]):
+		add_team_medals(i)
+	
 
 if not os.path.isdir("outputs"):
 	os.mkdir("outputs")
